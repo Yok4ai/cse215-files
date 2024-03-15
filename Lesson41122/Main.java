c780bc65fcc4404cca4a462dd3face3c7c4a1ffe
Yok4ai <148655260+Yok4ai@users.noreply.github.com>
2024-03-15 22:01:59 +0600

    folder rename

--- Lesson41122/Main.java
+++
@@ -1,9 +0,0 @@
-package Lesson41122;
-
-public class Main {
-    public static void main(String[] args) {
-    Polygon p = new Triangle(3,2,3,4);
-        System.out.println("Area= " + p.area());
-        System.out.println("Perimeter= " + p.perimeter());
-    }
-}
